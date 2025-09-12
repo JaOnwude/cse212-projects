55190b4439f0b602956f4116ebe724d5923e9bbd
JaOnwude <ojamesuchenna61@gmail.com>
2025-09-12 21:50:44 +0100

    added weel02 assignment

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -40,7 +40,11 @@ public class TakingTurnsQueue
         else
         {
             Person person = _people.Dequeue();
-            if (person.Turns > 1)
+            if (person.Turns == 0 || person.Turns < 0) // this is for infinite turns
+            {
+                _people.Enqueue(person);
+            }
+            else if (person.Turns > 1)
             {
                 person.Turns -= 1;
                 _people.Enqueue(person);
